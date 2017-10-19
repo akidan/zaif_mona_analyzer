f6d30902a9d11e62dd39fcb250916ebedfecbd15
zhiyue.wang <zhiyue.wang@rakuten.com>
2017-10-20 02:40:13 +0900

    add mona_sh.sh

---
+++ mona_sh.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+while : 
+do
+  cat ~/life/mona_data_latest.txt
+  i=60
+  while [ $i -gt 0 ] 
+  do
+    i=$(($i - 1))
+    echo -ne "`date +%H:%M:%S` Refresh after $i s\r"
+    sleep 1;
+  done
+done
