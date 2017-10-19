55a23354adcd560dfb77e1043c5d9931e2ec1654
akidan <zhiyue7@gmail.com>
2017-10-20 03:09:29 +0900

    fix interval

--- mona_sh.sh
+++ mona_sh.sh
@@ -2,11 +2,11 @@
 while : 
 do
   cat ~/Workspaces/mona_data_latest.txt
-  i=60
+  i=59
   while [ $i -gt 0 ] 
   do
     i=$(($i - 1))
-    echo -ne "`date +%H:%M:%S` Refresh after $i s\r"
+    echo -ne "`date +%H:%M:%S` Refresh after $i s \r"
     sleep 1;
   done
 done
